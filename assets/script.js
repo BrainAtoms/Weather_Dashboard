3cb3e3b2df3cf3fec99a0d72a6a54323968ae847
Brain Atoms <badams6453@gmail.com>
2023-03-03 14:24:42 -0500

    changed CSS and prevented duplicates from being added to city array

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,40 @@
+let cityArr = [];
+
+window.onload = function() {
+  document.getElementById('form1').onsubmit = function() {
+    var list = document.getElementById(this.getAttribute('data-list')); // only needed if more than one form or list
+    var listItem = document.createElement("li");
+    var inputItem = this.cityInput;
+    listItem.innerText = cityInput.value;
+    list.appendChild(listItem);
+    inputItem.select();
+    inputItem.focus();
+    return false; // stop submission
+  }
+  document.getElementById("cityInput").focus();
+}
+
+
+
 function GetInfo() {
-  const newName = document.getElementById("cityInput");
+  const newName = document.getElementById("cityInput").value;
   const cityName = document.getElementById("cityName");
-  cityName.innerHTML = "--" + newName.value + "--";
+  let cityUnique = [];
+  cityArr.forEach((element) => {
+    if (!cityUnique.includes(element)) {
+      cityUnique.push(element);
+    }
+  });
+  if (!cityArr.includes(newName)) {
+  cityArr.push(newName)
+  }
+  console.log(cityUnique)
+  console.log(cityArr)
+  cityName.innerHTML = newName;
 
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      newName.value +
+      newName +
       "&units=imperial&appid=bcd3d44da3112d7e38199a37b39882b7"
   )
     .then((response) => response.json())
@@ -31,17 +60,19 @@ function GetInfo() {
     })
 
     .catch((err) => alert("Something Went Wrong"));
-}
 
+    console.log(newName)
 
-function addCity() {
-  const newName = document.getElementById("cityInput");
-  document.getElementById("newCity").innerHTML = newName.value; 
 }
 
 function DefaultScreen() {
   document.getElementById("cityInput").defaultValue = "Atlanta";
+  document.getElementById("cityName").defaultValue = "Atlanta"; 
   GetInfo();
+}
+
+function addCity(){
+
 }
 
 var d = new Date();
@@ -69,3 +100,16 @@ for (i = 0; i < 5; i++) {
 
 currentDay = dayjs();
 $("#currentDay").text(currentDay.format("dddd MMM D, YYYY"));
+
+
+//create array of city names
+
+function selectCity(cityName) {
+  //sets city name
+  //searches for and displays weather
+  console.log('selectCity')
+  console.log(cityName)
+}
+
+var cityName = document.getElementById("list").addEventListener
+
