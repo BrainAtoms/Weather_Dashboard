ab42ef55aa83f0fe162f24706f022c56c41b2d50
Brain Atoms <badams6453@gmail.com>
2023-03-03 18:38:18 -0500

    made list items clickable and able to show weather conditions

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,53 @@
 let cityArr = [];
+var d = new Date();
+var weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
-window.onload = function() {
-  document.getElementById('form1').onsubmit = function() {
-    var list = document.getElementById(this.getAttribute('data-list')); // only needed if more than one form or list
-    var listItem = document.createElement("li");
-    var inputItem = this.cityInput;
-    listItem.innerText = cityInput.value;
-    list.appendChild(listItem);
-    inputItem.select();
-    inputItem.focus();
-    return false; // stop submission
-  }
-  document.getElementById("cityInput").focus();
+function addToList(newName) {
+  console.log(this)
+  console.log("adding to list");
+  var list = document.getElementById("list"); // only needed if more than one form or list
+  var listItem = document.createElement("li");
+  var inputItem = this.cityInput;
+  listItem.innerText = newName;
+  listItem.addEventListener('click', onCityListItemClick)
+  list.appendChild(listItem);
+  inputItem.select();
+  inputItem.focus();
+  return false; // stop submission
 }
 
+function onCityListItemClick(e) {
+  console.log(e.target.innerHTML)
+  console.log(e.target.value)
+  const city = e.target.innerHTML
+  document.getElementById("cityInput").value = city
+  GetInfo();
+}
 
-
-function GetInfo() {
+function GetInfo(event) {
+  if (event) {
+    event.preventDefault()
+  }
   const newName = document.getElementById("cityInput").value;
   const cityName = document.getElementById("cityName");
-  let cityUnique = [];
-  cityArr.forEach((element) => {
-    if (!cityUnique.includes(element)) {
-      cityUnique.push(element);
-    }
-  });
   if (!cityArr.includes(newName)) {
-  cityArr.push(newName)
+    console.log("city not already in list");
+    cityArr.push(newName);
+    console.log(cityArr);
+    addToList(newName);
   }
-  console.log(cityUnique)
-  console.log(cityArr)
-  cityName.innerHTML = newName;
+  console.log("city array");
+  console.log(cityArr);
+  // cityName.innerHTML = newName;
 
   fetch(
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -60,32 +77,15 @@ function GetInfo() {
     })
 
     .catch((err) => alert("Something Went Wrong"));
-
-    console.log(newName)
-
 }
 
 function DefaultScreen() {
+  console.log("default");
   document.getElementById("cityInput").defaultValue = "Atlanta";
-  document.getElementById("cityName").defaultValue = "Atlanta"; 
+  document.getElementById("cityName").defaultValue = "Atlanta";
+  document.getElementById("form1").onsubmit = GetInfo;
   GetInfo();
 }
-
-function addCity(){
-
-}
-
-var d = new Date();
-var weekday = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
 
 function CheckDay(day) {
   if (day + d.getDay() > 6) {
@@ -100,16 +100,3 @@ for (i = 0; i < 5; i++) {
 
 currentDay = dayjs();
 $("#currentDay").text(currentDay.format("dddd MMM D, YYYY"));
-
-
-//create array of city names
-
-function selectCity(cityName) {
-  //sets city name
-  //searches for and displays weather
-  console.log('selectCity')
-  console.log(cityName)
-}
-
-var cityName = document.getElementById("list").addEventListener
-
